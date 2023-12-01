b016042930c71ae7997e443555103bc1fa912d42
virginie.bm13@icloud.com <t.tizio@mailbox.org>
2023-12-01 17:17:14 +0100

    1erjet

---
+++ script.js
@@ -0,0 +1,4 @@
+const questionContainer = document.querySelector('.box');
+
+questionContainer.addEventListener("click",() => { questionContainer.style.opacity = "75%";
+questionContainer.style.borderRadius = "20%"});
